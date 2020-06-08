f9ce091dc7987cfb134f0dc02ff8a4568df47255
tsutsutaku <taku_0817_9@yahoo.co.jp>
2020-06-08 15:53:33 +0900

    add Posts and Likes models

--- positterapp/models.py
+++ positterapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Posts(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    positive_score = models.IntegerField()
+
+
+
+class Likes(models.Model):
+    user_id = models.ForeignKey(User, verbose_name='id', on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Posts, verbose_name='id', on_delete=models.CASCADE)
+
