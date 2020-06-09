f4f487d9b1b794781089c570f3cf7d6d9ec7c868
tsutsutaku <taku_0817_9@yahoo.co.jp>
2020-06-09 13:33:53 +0900

    add Profile to models

--- positterapp/models.py
+++ positterapp/models.py
@@ -35,3 +35,10 @@ class Reply(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                related_name="profile")
+    display_name = models.CharField(max_length=20)
--- positterapp/views.py
+++ positterapp/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views import generic
-from .models import Post, Like
+from .models import Post, Like, Profile
 
 # Create your views here.
 
@@ -17,21 +17,27 @@ def signupfunc(request):
     if request.method == 'POST':
         username2 = request.POST['username']
         password2 = request.POST['password']
+        displayname = request.POST['displayname']
+
         try:
             User.objects.get(username=username2)
-            return render(request, 'signup.html', {'error':'このユーザーは登録されています'})
+            return render(request, 'signup.html', {'error': 'このユーザーは登録されています'})
         except:
             user = User.objects.create_user(username2, '', password2)
-            login(request, authenticate(request, username=username2, password=password2))
+            login(
+                request,
+                authenticate(request, username=username2, password=password2))
+
+            Profile.objects.create(display_name=displayname, user=user)
+            user.save()
             return redirect('login')
     return render(request, 'signup.html')
-
 
 
 def loginfunc(request):
     if request.user.is_authenticated:
         return redirect('home')
-        
+
     if request.method == 'POST':
         username2 = request.POST['username']
         password2 = request.POST['password']
@@ -42,7 +48,8 @@ def loginfunc(request):
             return redirect('home')
         else:
             # Return an 'invalid login' error message.
-            return render(request, 'login.html', {'error':'無効なログインです。ユーザーネームかパスワードを確認してください。'})
+            return render(request, 'login.html',
+                          {'error': '無効なログインです。ユーザーネームかパスワードを確認してください。'})
 
     return render(request, 'login.html')
 
@@ -50,20 +57,18 @@ def loginfunc(request):
 def indexfunc(request):
     if request.user.is_authenticated:
         return redirect('home')
-    
+
     else:
         return render(request, 'index.html')
 
 
 def homefunc(request):
     object_list = Post.objects.all()
-    if  request.user.is_authenticated:
-        return render(request, 'home.html', {'object_list':object_list})
+    if request.user.is_authenticated:
+        return render(request, 'home.html', {'object_list': object_list})
 
     else:
         return redirect('index')
-        
-
 
 
 def logoutfunc(request):
