541cd1f07394194131bb64c4916c49170d278f09
tsutsutaku <taku_0817_9@yahoo.co.jp>
2020-06-10 15:44:26 +0900

    ADD created_at in posieet

--- positterapp/models.py
+++ positterapp/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.utils import timezone
 
 
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
-    positive_score = models.IntegerField(null=True, )
+    positive_score = models.IntegerField(null=True)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return str(self.id)
